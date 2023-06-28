434be5e164f0128c73df1089d9966eebf3269c6e
joshbouncesecurity <josh@bouncesecurity.com>
2023-06-28 20:54:02 +0300

    Update semgrep and depcheck outputs

--- run_semgrep_docker.sh
+++ run_semgrep_docker.sh
@@ -1,3 +1,5 @@
+# Note that this does no run pro rules to avoid having to do semgrep login
+
 docker run -v ${PWD}:/src returntocorp/semgrep scan --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.txt'
 
 docker run -v ${PWD}:/src returntocorp/semgrep scan --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.sarif' --sarif
