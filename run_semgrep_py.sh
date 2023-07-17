da38034a42edf9c517c63e3889c13b58a1c4d4c3
joshbouncesecurity <josh@bouncesecurity.com>
2023-07-17 12:46:50 +0300

    Add updated results

--- run_semgrep_py.sh
+++ run_semgrep_py.sh
@@ -2,9 +2,11 @@
 
 # Note that this does not run pro rules to avoid having to do semgrep login
 
-declare -a StringArray=("-o semgrep_results.txt" "--sarif -o semgrep_results.sarif" "--json -o semgrep_results.json"  )
+declare -a StringArray=("--no-force-color -o semgrep_results.txt" "--sarif -o semgrep_results.sarif" "--json -o semgrep_results.json"  )
 
 for outFormat in "${StringArray[@]}"
 do
   semgrep --verbose --oss-only --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' $outFormat 
 done
+
+cat semgrep_results.txt | sed 's/\x1b\[[0-9;]*m//g' > semgrep_results.clean.txt
