ef14698f31b9ccd2aadd66a9d78967818eeb9df2
Josh Grossman (Bounce Security) <97975715+joshbouncesecurity@users.noreply.github.com>
2023-07-17 17:04:17 +0300

    Update output routines and outputs

--- run_semgrep_docker.sh
+++ run_semgrep_docker.sh
@@ -2,9 +2,12 @@
 
 # Note that this does not run pro rules to avoid having to do semgrep login
 
-docker run -v ${PWD}:/src returntocorp/semgrep scan --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.txt'
+sudo apt install aha
 
-docker run -v ${PWD}:/src returntocorp/semgrep scan --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.sarif' --sarif
+docker run -v ${PWD}:/src returntocorp/semgrep semgrep --verbose --force-color --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.txt'
 
-docker run -v ${PWD}:/src returntocorp/semgrep scan --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.json' --json
+docker run -v ${PWD}:/src returntocorp/semgrep semgrep --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.sarif' --sarif
 
+docker run -v ${PWD}:/src returntocorp/semgrep semgrep --verbose --config p/java --config r/contrib.owasp.java --config r/generic --include='*.java' --metrics='off' --severity='WARNING' --severity='ERROR' -o '/src/semgrep_results.json' --json
+
+cat semgrep_results.txt | aha --black > semgrep_results.html
