86ed2f8ccb7d9290fbc5718fad58525cc72ba4f7
Josh Grossman (Bounce Security) <97975715+joshbouncesecurity@users.noreply.github.com>
2023-07-25 08:04:35 +0300

    Trim rules

--- run_semgrep_py.sh
+++ run_semgrep_py.sh
@@ -8,7 +8,7 @@ declare -a StringArray=("--force-color -o semgrep_results.txt" "--sarif -o semgr
 
 for outFormat in "${StringArray[@]}"
 do
-  semgrep --oss-only --config "p/gitlab" --config p/java --config r/contrib.owasp.java --metrics='off' --severity='WARNING' --severity='ERROR'  $outFormat 
+  semgrep --oss-only --config "p/gitlab" --config p/java --config r/contrib.owasp.java --metrics='off' --severity='WARNING' --severity='ERROR' --exclude-rule gitlab.find_sec_bugs.INFORMATION_EXPOSURE_THROUGH_AN_ERROR_MESSAGE-1 --exclude-rule gitlab.eslint.detect-object-injection --exclude-rule gitlab.eslint.detect-non-literal-regexp --exclude-rule gitlab.find_sec_bugs.CUSTOM_INJECTION-2 --exclude-rule gitlab.eslint.detect-object-injection --exclude-rule gitlab.find_sec_bugs.SQL_INJECTION_SPRING_JDBC-1.SQL_INJECTION_JPA-1.SQL_INJECTION_JDO-1.SQL_INJECTION_JDBC-1.SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE-1 --exclude-rule gitlab.find_sec_bugs.SQL_INJECTION_SPRING_JDBC-1.SQL_INJECTION_JPA-1.SQL_INJECTION_JDO-1.SQL_INJECTION_JDBC-1.SQL_NONCONSTANT_STRING_PASSED_TO_EXECUTE-1.SQL_INJECTION-1.SQL_INJECTION_HIBERNATE-1.SQL_INJECTION_VERTX-1.SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING-1 $outFormat 
 done
 
 cat semgrep_results.txt | aha --black > semgrep_results.html
